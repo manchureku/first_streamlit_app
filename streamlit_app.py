ed4d90f495f470cdfcb96470de82423f99cfb604
manchureku <146965187+manchureku@users.noreply.github.com>
2023-10-04 16:24:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title ('The New, Healthy Diner in the Town for my Parents....THIS IS THE TEST')
+streamlit.title ('My Parents New Healthy Diner')
+streamlit.header (' Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-bolied Free Range Egg')
