7bbb576171db18e92b65a1f4ad7a9d5a6ce59bd1
manchureku <146965187+manchureku@users.noreply.github.com>
2023-10-09 17:44:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,10 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit_load_list contains:")
 streamlit.dataframe(my_data_rows)
-#streamlit.write('Thanks for adding ', add_my_fruit)
-#my_cur.execute("insert into fruit_load_list_values('from Streamlit')")
-def insert_row_snowflake(new_fruit):
- with my_cnx.cursor() as my_cur:
-  my_cur.execute("insert into fruit_load_list_values('jackfruit'+'papaya'+'guava'+'kiwi')")
-  return "Thanks for adding" + new_fruit
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list_values('from Streamlit')")
+#def insert_row_snowflake(new_fruit):
+# with my_cnx.cursor() as my_cur:
+ # my_cur.execute("insert into fruit_load_list_values('jackfruit'+'papaya'+'guava'+'kiwi')")
+ # return "Thanks for adding" + new_fruit
  
