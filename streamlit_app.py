e478d120df01ab3793a6cf55a439cd9909b12192
manchureku <146965187+manchureku@users.noreply.github.com>
2023-10-10 09:12:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("insert into fruit_load_list values 'jackfruit','papaya','kiwi','guava'")
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit_load_list contains:")
